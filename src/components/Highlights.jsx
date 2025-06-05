1cb3456d9e982431a5c6353346c3ad5ec0e8bbfd
Mayank Mohit Agarwal <mayankmohitagarwal7@gmail.com>
2025-06-05 18:54:15 +0530

    Completed Hero and Highlights

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,61 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
+import { useRef } from 'react';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
+import VideoCarousel from './VideoCarousel';
+
+const Highlights = () => {
+    const scrollRef = useRef();
+    const boxes = gsap.utils.toArray(scrollRef.current.children);
+
+    useGSAP(() => {
+        gsap.to('#title', {
+            opacity: 1,
+            y: 0,
+            scrollTrigger: {
+                trigger: '#title',
+                start: 'bottom bottom',
+                end: 'top 20%',
+                scrub: true,
+            },
+        })
+        boxes.map((box, _) => {
+            gsap.to(box, {
+                opacity: 1, y: 0, duration: 1, stagger: 0.25,
+                scrollTrigger: {
+                    trigger: box,
+                    start: 'bottom bottom',
+                    end: 'top 20%',
+                    scrub: true,
+                },
+        })})
+    }, [])
+
+  return (
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className=" screen-max-width max-w-max">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
+
+          <div className="flex flex-wrap items-end gap-5" ref={scrollRef}>
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+
+        <VideoCarousel />
+      </div>
+    </section>
+  )
+}
+
+export default Highlights
