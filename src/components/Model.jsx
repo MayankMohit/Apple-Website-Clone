1e4beaa22dd219e4a3e395ee6ce3d5b27e60542f
Mayank Mohit Agarwal <mayankmohitagarwal7@gmail.com>
2025-08-07 22:23:38 +0530

    Started 3DModel Section

---
+++ src/components/Model.jsx
@@ -0,0 +1,29 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import ModelView from "./ModelView";
+
+const Model = () => {
+    useGSAP(() => {
+        gsap.to('#heading', {
+            opacity: 1,
+            y: 0,
+        })
+    }, [])
+
+  return (
+      <section className='commom-padding'>
+          <div className='screen-max-width '>
+              <h1 className='section-heading' id='heading'>
+                  Take a closer look.
+              </h1>
+              <div className="flex flex-col items-center mt-5">
+                  <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+                      <ModelView />
+                  </div>
+              </div>
+          </div>
+    </section>
+  )
+}
+
+export default Model
